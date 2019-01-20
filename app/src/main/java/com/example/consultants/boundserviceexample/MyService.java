ea179655f59af8d1fa456b1679df721d51879bc6
princeessel <43561228+princeessel@users.noreply.github.com>
2019-01-20 18:53:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/consultants/boundserviceexample/MyService.java
@@ -0,0 +1,13 @@
+package com.example.consultants.boundserviceexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
