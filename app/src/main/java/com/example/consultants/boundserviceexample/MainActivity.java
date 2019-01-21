0031ad3cb039a3a3a7c3627a4297679b62a906e2
princeessel <43561228+princeessel@users.noreply.github.com>
2019-01-20 21:47:05 -0500

    Basic Bound Service Implementation.

--- app/src/main/java/com/example/consultants/boundserviceexample/MainActivity.java
+++ app/src/main/java/com/example/consultants/boundserviceexample/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.consultants.boundserviceexample;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+ TextView textView;
+ boolean isBind=false;
+ MyService myService;
+    private Object service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView2);
+        Intent intent=new Intent(this,MyService.class);
+        bindService(intent,serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void getClientServiceMessage(View view){
+
+        String message;
+        message=myService.getClientMessage();
+        textView.setText(message);
+
+    }
+
+
+    public void getServerServiceMessage(View view){
+        String message;
+        message=myService.getServerMessage();
+        textView.setText(message);
+
+
+    }
+
+    private ServiceConnection serviceConnection=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            MyService.InterfaceService interfaceService= (MyService.InterfaceService) service;
+            myService=interfaceService.getService();
+            isBind=true;
+
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+  isBind=false;
+
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isBind){
+            unbindService(serviceConnection);
+            isBind=false;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/consultants/boundserviceexample/MyService.java
+++ app/src/main/java/com/example/consultants/boundserviceexample/MyService.java
@@ -2,12 +2,33 @@ package com.example.consultants.boundserviceexample;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MyService extends Service {
-
+private final IBinder iBinder= new InterfaceService();
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return iBinder;
     }
+
+    public class InterfaceService extends Binder{
+
+        MyService getService(){
+
+            return MyService.this;
+        }
+
+    }
+    public String getClientMessage(){
+
+
+        return "Client is Connected";
+    }
+
+    public String getServerMessage(){
+        return "Message Saved in Server";
+
+    }
+
 }
